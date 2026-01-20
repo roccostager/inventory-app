c849e420e47c748516bdbe52a5379f985de5ee99
roccostager <99860439+roccostager@users.noreply.github.com>
2026-01-20 16:35:45 +0100

    Create add functionality using insertItem

--- app.js
+++ app.js
@@ -9,9 +9,22 @@ const { getAllItems, insertItem } = require('./db/query');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     res.render('index', { items: await getAllItems() });
+})
+
+app.get('/add', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'add.html'));
+})
+app.post('/add', async (req, res) => {
+    await insertItem({
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+    });
+    res.redirect('/');
 })
 
 app.listen(port, error => {
